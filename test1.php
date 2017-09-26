af7a73ccfe943b1fc22ae93f0db602523796a3ff
dyh <2281711141@qq.com>
2017-09-26 14:06:10 +0800

    1

--- test1.php
+++ test1.php
@@ -3,9 +3,13 @@
 	/*$redis->mset(array('my_name'=>'董亚华','her_name'=>'韩佳娜'));
 	//echo '生存时间' . $redis->ttl('my_name');
 	echo '姓名:' . $redis->get('my_name');*/
-	$redis->set('key1',1);
+	/*$redis->set('key1',1);
 	echo $redis->get('key1');
 	echo '<br/>';
 	$redis->incr('key1');
-	echo $redis->get('key1');
+	echo $redis->get('key1');*/
+	$redis->lpush('our_name','董亚华');
+	echo '我的名字' . $redis->get('our_name');
+	$redis->lpush('our_name','韩佳娜');
+	echo '我们的名字' . $redis->get('our_name');
 ?>
