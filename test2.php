8076bb84ee902b85cf41abed7b54f102e9d29819
dyh <2281711141@qq.com>
2017-09-26 15:38:53 +0800

    1

--- test2.php
+++ test2.php
@@ -1,7 +1,9 @@
 <?php
 	require 'common.php';
 	//echo '生存时间' . $redis->ttl('my_name');
-	echo '姓名:' . $redis->get('her_name');
+	//echo '姓名:' . $redis->get('her_name');
 	//$redis->delete('my_name');
 	//echo '姓名:' . $redis->get('my_name');
+	$redis->lset('number', 2, 100);
+	print_r($redis->lrange('number',0,-1));
 ?>
