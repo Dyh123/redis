0e7edda0d12861fbde2a1b78cd8aec83b06e2032
dyh <2281711141@qq.com>
2017-09-26 12:28:09 +0800

    re

--- test1.php
+++ test1.php
@@ -1,6 +1,7 @@
 <?php
 	$redis = new Redis();
 	$redis->connect('127.0.0.1',6379);
-	$redis->set('my_name','董亚华');
+	$redis->setex('my_name',120,'董亚华');
+	echo '生存时间' . $redis->ttl('my_name');
 	echo '姓名:' . $redis->get('my_name');
 ?>
