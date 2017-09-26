bdeda800d68d92f9c72f26a3a244dd285d76efee
dyh <2281711141@qq.com>
2017-09-26 13:59:49 +0800

    1

--- test1.php
+++ test1.php
@@ -1,6 +1,6 @@
 <?php
 	require 'common.php';
-	$redis->set('my_name','董亚华');
+	$redis->mset(array('my_name'=>'董亚华','her_name'=>'韩佳娜'));
 	//echo '生存时间' . $redis->ttl('my_name');
 	echo '姓名:' . $redis->get('my_name');
 ?>
--- test2.php
+++ test2.php
@@ -1,7 +1,7 @@
 <?php
 	require 'common.php';
 	//echo '生存时间' . $redis->ttl('my_name');
-	echo '姓名:' . $redis->get('my_name');
-	$redis->delete('my_name');
-	echo '姓名:' . $redis->get('my_name');
+	echo '姓名:' . $redis->get('her_name');
+	//$redis->delete('my_name');
+	//echo '姓名:' . $redis->get('my_name');
 ?>
