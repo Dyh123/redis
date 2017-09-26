ecca3e9b3760ff0ec11333dfb2752cd4f09b6f26
dyh <2281711141@qq.com>
2017-09-26 14:03:08 +0800

    1

--- test1.php
+++ test1.php
@@ -1,6 +1,11 @@
 <?php
 	require 'common.php';
-	$redis->mset(array('my_name'=>'董亚华','her_name'=>'韩佳娜'));
+	/*$redis->mset(array('my_name'=>'董亚华','her_name'=>'韩佳娜'));
 	//echo '生存时间' . $redis->ttl('my_name');
-	echo '姓名:' . $redis->get('my_name');
+	echo '姓名:' . $redis->get('my_name');*/
+	$redis->set('key1',1);
+	echo $redis->get('key1');
+	echo '<br/>';
+	$redis->incr('key1');
+	echo $redis->get('key1');
 ?>
