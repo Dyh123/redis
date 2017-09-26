e922b6188446592458cf8f3f640daf0378ca86a0
dyh <2281711141@qq.com>
2017-09-26 13:56:48 +0800

    1

--- test1.php
+++ test1.php
@@ -1,6 +1,6 @@
 <?php
 	require 'common.php';
-	$redis->setex('my_name',60,'董亚华');
-	echo '生存时间' . $redis->ttl('my_name');
+	$redis->set('my_name',60,'董亚华');
+	//echo '生存时间' . $redis->ttl('my_name');
 	echo '姓名:' . $redis->get('my_name');
 ?>
--- test2.php
+++ test2.php
@@ -1,5 +1,7 @@
 <?php
 	require 'common.php';
-	echo '生存时间' . $redis->ttl('my_name');
+	//echo '生存时间' . $redis->ttl('my_name');
+	echo '姓名:' . $redis->get('my_name');
+	$redis->delete('my_name');
 	echo '姓名:' . $redis->get('my_name');
 ?>
