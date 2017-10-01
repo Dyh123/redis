ece573d7071ed4ea0d93030f09f1b69f16492cf1
dyh <2281711141@qq.com>
2017-10-01 18:07:11 +0800

    1

--- test1.php
+++ test1.php
@@ -12,10 +12,11 @@
 	echo '我的名字' . $redis->lpop('our_name');
 	$redis->lpush('our_name','韩佳娜');
 	echo '我们的名字' . $redis->lpop('our_name');*/
-	$redis->flushDB();
+	/*$redis->flushDB();
 	for($i = 1;$i<10;$i++){
 		$redis->lpush('number', $i);
 	}
 	echo  '列表中共有 :' . $redis->lsize('number') . '个元素';
-	echo '元素为:' . print_r($redis->lrange('number',0,-1));
+	echo '元素为:' . print_r($redis->lrange('number',0,-1));*/
+	$redis->delete('guest');
 ?>
