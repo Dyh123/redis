06b4caacc3fe7d457e800f827752d93adedb101f
dyh <2281711141@qq.com>
2017-10-01 15:43:13 +0800

    11

--- bf.php
+++ bf.php
@@ -1,7 +1,12 @@
 <?php
 	$conn = mysqli_connect('127.0.0.1','root','root','mydatabase') or die('数据库连接失败');
+	$conn->autocommit(false);
 	$sql = 'update article set type=1 where type=0 limit 1';
-	$result = $conn->query($sql)
-	echo $result;
+	$result = $conn->query($sql);
+	if($result){
+		$conn->commit();
+	}else{
+		$conn->rollback();
+	}
 
 ?>
