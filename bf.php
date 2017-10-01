f07719676ddd02b1fa7ffea0a8ae257341e9404c
dyh <2281711141@qq.com>
2017-10-01 16:19:20 +0800

    1

--- bf.php
+++ bf.php
@@ -1,9 +1,16 @@
 <?php
 	require 'common.php';
-	function up(){
+	$redis->flushDB();
+	for($i = 1;$i<10;$i++){
+		$redis->lpush('number', $i);
+	}
+	echo  '列表中共有 :' . $redis->lsize('number') . '个元素';
+	echo '元素为:' . print_r($redis->lrange('number',0,-1));
+	/*function up(){
 		if(!($key = $redis->get('key'))){
 			$redis->set('key',1);
 		}
+		echo $key;exit;
 		if($key==1){
 			$redis->set('key',2);
 			$conn = mysqli_connect('127.0.0.1','root','root','mydatabase') or die('数据库连接失败');
@@ -26,7 +33,7 @@
 	up();
 	while($redis->lsize('guest')){
 		up();
-	}
+	}*/
 	
 
 ?>
