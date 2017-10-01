ba9449ec69455a3ef6037fbf03780a5f323442b7
dyh <2281711141@qq.com>
2017-10-01 15:41:18 +0800

    d

--- test2.php
+++ test2.php
@@ -4,6 +4,12 @@
 	//echo '姓名:' . $redis->get('her_name');
 	//$redis->delete('my_name');
 	//echo '姓名:' . $redis->get('my_name');
-	$redis->lset('number', 2, 100);
-	print_r($redis->lrange('number',0,-1));
+	/*$redis->lset('number', 2, 100);
+	print_r($redis->lrange('number',0,-1));*/
+	if(!isset($a)){
+		static $a = 1;
+	}
+	echo $a;
+	echo '<hr/>';
+	$a = 2;
 ?>
