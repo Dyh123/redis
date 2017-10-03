275b656e123d0c6f23e8d98839e53badd3982874
dyh <2281711141@qq.com>
2017-10-03 18:50:00 +0800

    1

--- bf.php
+++ bf.php
@@ -8,6 +8,7 @@
 		if(($redis->get('key'))==1){
 			$redis->set('key',2);
 			$conn = mysqli_connect('127.0.0.1','root','admin123','mydatabase') or die('数据库连接失败');
+			echo 1;exit;
 			$conn->autocommit(false);
 			$sql = 'update article set type=1 where type=0 limit 1';
 			$result = $conn->query($sql);
