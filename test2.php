c6eac314bdad65161a297492c556bb9d7d6f7a4d
dyh <2281711141@qq.com>
2017-10-07 21:54:48 +0800

    1

--- test2.php
+++ test2.php
@@ -7,7 +7,7 @@
 	/*$redis->lset('number', 2, 100);
 	print_r($redis->lrange('number',0,-1));*/
 	//echo $redis->lsize('guest');
-	$redis->incr('age');
+	$redis->incrby('age',10);
 	echo $redis->get('age');
 	echo '<hr>';
 	echo $redis->get('age2');
