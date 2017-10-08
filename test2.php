dedbd79d2cb2baf2239d6f4c505baeda5a63f7d2
dyh <2281711141@qq.com>
2017-10-08 13:07:27 +0800

    1

--- test2.php
+++ test2.php
@@ -7,12 +7,13 @@
 	/*$redis->lset('number', 2, 100);
 	print_r($redis->lrange('number',0,-1));*/
 	//echo $redis->lsize('guest');
-	$redis->incrby('age',10);
+	/*$redis->incrby('age',10);
 	echo $redis->ttl('age');
 	echo '<hr>';
 	echo $redis->get('age2');
 	echo '<hr>';
 	$redis->rename('age2','age3');
 	echo '<hr>';
-	echo $redis->get('age3');
+	echo $redis->get('age3');*/
+	echo $redis->lpop('class');
 ?>
